ee1478a459c25900764eb97ebe8621a5d50b9a83
zhangzhang2008 <471750090@qq.com>
2021-04-02 21:50:22 +0800

    初始导入

--- Module1/src/com/tx/HelloWorld.java
+++ Module1/src/com/tx/HelloWorld.java
@@ -12,6 +12,40 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         test();
+        System.out.printf("");
+        String[] arry = new String[]{"zs", "ls", "ww"};
+        for (int i = 0; i < arry.length; i++) {
+            System.out.println(arry[i]);
+        }
+
+        for (String a : arry) {
+            System.out.println(a);
+        }
+
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for (Object o : list) {
+            System.out.println(o);
+        }
+
+        if (list == null) {
+
+        }
+
+        if (list != null) {
+
+        }
+
+        if (list == null) {
+
+        }
+
+        if (list != null) {
+
+        }
+
 
     }
 
---
+++ Module2/src/com/tx/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.tx;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("Hello World!!!");
+    }
+}
