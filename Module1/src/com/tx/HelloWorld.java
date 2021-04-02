50713e2d4e2a794a7a214a8731507977b6bac2b2
zhangzhang2008 <471750090@qq.com>
2021-04-02 21:48:24 +0800

    初始导入

---
+++ Module1/src/com/tx/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.tx;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        test();
+
+    }
+
+    private static void test() {
+        System.out.print("Hello World!!!");
+        ArrayList list = new ArrayList();
+        System.out.println(0.05+0.01==0.06);
+    }
+
+    public String test1(int state, String str) {
+        return "a";
+    }
+}
---
+++ Module1/src/com/tx/Test1.java
@@ -0,0 +1,11 @@
+package com.tx;
+
+/**
+ * @author zhm
+ * @create 2021/4/1-23:46
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+    }
+}
