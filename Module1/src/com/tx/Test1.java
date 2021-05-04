25e842f90531c004bf2659cbf55f8e23bdbfea03
zhangzhang2008 <471750090@qq.com>
2021-05-05 01:53:04 +0800

    测试

--- Module1/src/com/tx/Test1.java
+++ Module1/src/com/tx/Test1.java
@@ -1,11 +1,18 @@
 package com.tx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author zhm
  * @create 2021/4/1-23:46
  */
 public class Test1 {
 
+    private int id;
+    private String name;
+
     public static void main(String[] args) {
     }
+
 }
