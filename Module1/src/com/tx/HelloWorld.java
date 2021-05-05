b43c3414f0449f0fcfea2927c6dccd2b0670d85a
zhangzhang2008 <471750090@qq.com>
2021-05-05 13:34:51 +0800

    测试

--- Module1/src/com/tx/HelloWorld.java
+++ Module1/src/com/tx/HelloWorld.java
@@ -1,61 +1,29 @@
 package com.tx;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HelloWorld {
 
-
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) {
 
         test();
-        System.out.printf("");
         String[] arry = new String[]{"zs", "ls", "ww"};
-        for (int i = 0; i < arry.length; i++) {
-            System.out.println(arry[i]);
-        }
-
         for (String a : arry) {
             System.out.println(a);
         }
 
-        ArrayList<Object> list = new ArrayList<>();
+        List<Object> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
         for (Object o : list) {
             System.out.println(o);
         }
-
-        if (list == null) {
-
-        }
-
-        if (list != null) {
-
-        }
-
-        if (list == null) {
-
-        }
-
-        if (list != null) {
-
-        }
-
-
     }
 
     private static void test() {
         System.out.print("Hello World!!!");
-        ArrayList list = new ArrayList();
         System.out.println(0.05+0.01==0.06);
-    }
-
-    public String test1(int state, String str) {
-        return "a";
     }
 }
